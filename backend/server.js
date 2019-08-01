5a98f3bb4346b27e58dfca47b61a71f2367a375d
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-08-02 00:06:46 +0530

    Backend: MongoDB Atlas Connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
+// Require
+
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 require("dotenv").config();
 
@@ -9,6 +12,18 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Setup DB Connection
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("Connected to MongoDB database!");
+});
+
+// Setup Listener
+
 app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
