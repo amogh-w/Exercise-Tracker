fe189ca667be80614b6b402e428f7d1c581b7583
Amogh Warkhandkar <amoghw2025@gmail.com>
2019-08-02 11:56:31 +0530

    Backend: Added Advanced Exercises Routes

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -21,4 +21,32 @@ router.route("/add").post((req, res) => {
     .catch(err => res.status(400).json(`Err: ${err}`));
 });
 
-module.exports = router
+router.route("/:id").get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json(`Err: ${err}`));
+});
+
+router.route("/:id").delete((req, res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Exercise Deleted!"))
+    .catch(err => res.status(400).json(`Err: ${err}`));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username;
+      exercise.description = req.body.description;
+      exercise.duration = Number(req.body.duration);
+      exercise.date = Date.parse(req.body.date);
+
+      exercise
+        .save()
+        .then(() => res.json("Exercise Updated!"))
+        .catch(err => res.status(400).json(`Err: ${err}`));
+    })
+    .catch(err => res.status(400).json(`Err: ${err}`));
+});
+
+module.exports = router;
